8b9764c7aaa6f0d44f8e8435a066621cd4cbaa9a
DINESH KUMAR <dineshkumar.icon@gmail.com>
2021-03-22 19:25:56 +0530

    [dinesh] ADD:AttendanceCheck MODIFIED:Employee Attendance UPDATE:EMPLOYEE PRESENT

--- Employeewages.java
+++ Employeewages.java
@@ -1,6 +1,14 @@
 package employeewages;
+import java.util.Random;
 class Employeewages{
 public static void main(String[] args){
 System.out.println("Welcome to Employee");
+int Present=1;
+int empcheck=((int)(Math.random()*2)+0);
+if(empcheck == Present){
+System.out.println("Employee is Present");
+}else{
+System.out.println("Employee is Absent");
+  }
  }
 }
