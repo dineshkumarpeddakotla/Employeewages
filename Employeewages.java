b8fb40baba0468f2c2b5c507c68487403b2c5119
DINESH KUMAR <dineshkumar.icon@gmail.com>
2021-03-22 19:49:33 +0530

    [dinesh] ADD:Part_Time_Wages MODIFIED:If Statement UPDATE:Part_Time_Employee

--- Employeewages.java
+++ Employeewages.java
@@ -6,11 +6,14 @@ System.out.println("Welcome to Employee");
 int Present=1;
 int Wage_per_hr=20;
 int Full_Day_hrs=8;
+int Part_Time_hrs=4;
 int empcheck=((int)(Math.random()*2)+0);
 if(empcheck == Present){
 System.out.println("Employee is Present");
 int Daily_Employee_Wage = (Wage_per_hr*Full_Day_hrs);
+int Part_time_Employee_wage = (Wage_per_hr*Part_Time_hrs);
 System.out.println("Employee daily wage =" +Daily_Employee_Wage);
+System.out.println("Part time Employee wage =" +Part_time_Employee_wage);
 }else{
 System.out.println("Employee is Absent");
   }
